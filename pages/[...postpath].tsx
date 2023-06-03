59d6bfe5ffb160eec9d28be8df0092c06c8fafd0
mishalrafique1 <134150187+mishalrafique1@users.noreply.github.com>
2023-06-03 18:04:04 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://world1news.rf.gd/graphql"
+	const endpoint = "https://www.tellyupdates.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://world1news.rf.gd/` + encodeURI(path as string)
+					`https://www.tellyupdates.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
